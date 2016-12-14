3bfcbbcdfbdf61fea4d1ca8ba93390f448fa168b
bendunn135 <bendunn135@gmail.com>
2016-12-14 16:44:58 +0000

    Separate components for modifiers

--- app/containers/Foo.jsx
+++ app/containers/Foo.jsx
@@ -4,7 +4,7 @@ import Alert from '../library/components/alert/alert.jsx'
 export default class Foo extends React.Component {
     render() {
         return <div>
-          <Alert title="This is an alert heading">
+          <Alert title="This is an alert heading" type={3}>
             This is the alert content  
           </Alert>
         </div>;
--- app/library/components/alert/alert.jsx
+++ app/library/components/alert/alert.jsx
@@ -3,92 +3,132 @@ import {styled} from 'styletron-react';
 import tinycolor from 'tinycolor2';
 import styles from '../../theme/index.js';
 
-function getStateVariables(type) {
-  switch (type) {
-    case 1:
-      return {
-        color: tinycolor(styles.colors.brand.success).darken(30).toString(),
-        bg: tinycolor(styles.colors.brand.success).lighten(30).toString(),
-        border_color: tinycolor(styles.colors.brand.success).lighten(20).toString()
-      }
-    case 2:
-      return {
-        color: tinycolor(styles.colors.brand.danger).darken(30).toString(),
-        bg: tinycolor(styles.colors.brand.danger).lighten(30).toString(),
-        border_color: tinycolor(styles.colors.brand.danger).lighten(20).toString()
-      }
-    case 3:
-      return {
-        color: tinycolor(styles.colors.brand.info).darken(30).toString(),
-        bg: tinycolor(styles.colors.brand.info).lighten(30).toString(),
-        border_color: tinycolor(styles.colors.brand.info).lighten(20).toString()
-      }
-    case 4:
-      return {
-        color: tinycolor(styles.colors.brand.warning).darken(30).toString(),
-        bg: tinycolor(styles.colors.brand.warning).lighten(30).toString(),
-        border_color: tinycolor(styles.colors.brand.warning).lighten(20).toString()
-      }
-    default:
-      return {
-        bg: styles.colors.gray_lightest,
-        border_color: "transparent",
-        color: styles.colors.text_color
-      }
-  }
-}
 
-const type = 1;
-const _styles = getStateVariables(type);
+/* ==========================================================================
+   Styles
+========================================================================== */
 
-// .c-alert
-const Container = styled('div', () => ({
-  margin:         '5px 0 15px',
-  padding:        '8px 10px',
-  border:         `1px solid ${_styles.border_color}`,
-  background:     _styles.bg,
-  borderRadius:   styles.components.border_radius_base,
-  textAlign:      'left',
-  color:          _styles.color,
+/* Block styles
+========================================================================== */
+
+const Default = styled('div', () => ({
+  margin: '5px 0 15px',
+  padding: '8px 10px',
+  border: `1px solid transparent`,
+  background: styles.colors.gray_lightest,
+  borderRadius: styles.components.border_radius_base,
+  textAlign: 'left',
+  color: styles.colors.text_color,
   ':hover': {
     color: styles.colors.bright_blue,
   },
   '@media (min-width: 1024px)': {
-    padding: '16px 20px',
+    padding: '8px 20px',
   }
 }));
 
-// .c-alert__header
+
+/* Element styles
+========================================================================== */
+
 const Header = styled('div', () => ({
-  marginTop:      '1px',
-  marginBottom:   '2px',
-  fontSize:       styles.type.font_size_h4,
-  color:          'inherit'
+  marginTop: '1px',
+  marginBottom: '2px',
+  fontSize: styles.type.font_size_h4,
+  color: 'inherit'
 }));
 
-// .c-alert__icon
 const Icon = styled('div', () => ({
-  float:          'left',
-  margin:         '3px 10px 0 0',
-  fontSize:       '1em'
+  float: 'left',
+  margin: '3px 10px 0 0',
+  fontSize: '1em'
 }));
 
-// .c-alert__description
 const Description = styled('div', () => ({
   margin: 0
 }));
 
-export default class Label extends React.Component {
+
+/* Modifer styles
+========================================================================== */
+
+const Success = styled(Default, () => ({
+  border: `1px solid ${tinycolor(styles.colors.brand.success).lighten(20).toString()}`,
+  background: tinycolor(styles.colors.brand.success).lighten(30).toString(),
+  color: tinycolor(styles.colors.brand.success).darken(30).toString(),
+}));
+
+const Danger = styled(Default, () => ({
+  border: `1px solid ${tinycolor(styles.colors.brand.danger).lighten(20).toString()}`,
+  background: tinycolor(styles.colors.brand.danger).lighten(30).toString(),
+  color: tinycolor(styles.colors.brand.danger).darken(30).toString(),
+}));
+
+const Info = styled(Default, () => ({
+  border: `1px solid ${tinycolor(styles.colors.brand.info).lighten(20).toString()}`,
+  background: tinycolor(styles.colors.brand.info).lighten(30).toString(),
+  color: tinycolor(styles.colors.brand.info).darken(30).toString(),
+}));
+
+const Warning = styled(Default, () => ({
+  border: `1px solid ${tinycolor(styles.colors.brand.warning).lighten(20).toString()}`,
+  background: tinycolor(styles.colors.brand.warning).lighten(30).toString(),
+  color: tinycolor(styles.colors.brand.warning).darken(30).toString(),
+}));
+
+const ValidationError = styled(Danger, () => ({
+  display: "inline-block",
+  position: "relative",
+  width: "100%",
+  '::after': {
+    content: "Hello",  // "\25B2", // "▲", // 'content' appears to not be supported
+    position: "absolute",
+    top: "-12px",
+    left: "15px",
+    fontSize: "50px",
+    color: tinycolor(styles.colors.brand.danger).lighten(30).toString()
+  }
+}));
+
+
+
+/* ==========================================================================
+   React Component
+========================================================================== */
+
+class Label extends React.Component {
   render() {
-    return <Container>
-      <Header>{ this.props.title }</Header>
+    let {type, title, children} = this.props;
+    
+    const Alert = (() => {
+      switch(type) {
+        case 1:
+          return Success;
+        case 2:
+          return Danger;
+        case 3:
+          return Info;
+        case 4:
+          return Warning;
+        case 5:
+          return ValidationError;
+        default:
+          return Default;
+      }
+    })();
+    
+    return <Alert>
+      <Header>{ title }</Header>
       <Description>
-        { this.props.children }
+        { children }
       </Description>
-    </Container>;
+    </Alert>;
   }
 }
 
 Label.propTypes = {
-  title: React.PropTypes.string
+  title: React.PropTypes.string,
+  type: React.PropTypes.number
 };
+
+export default Label;
