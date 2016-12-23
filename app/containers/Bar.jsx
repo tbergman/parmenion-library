aebf500154cea766f65d331847b3884dfe3ce7f2
Ben Dunn <ben.dunn@parmenion.co.uk>
2016-12-23 13:00:33 +0000

    Button as bool

--- app/containers/Bar.jsx
+++ app/containers/Bar.jsx
@@ -3,6 +3,7 @@ import Accordion from '../library/components/Containers/Accordion/Accordion.jsx'
 import AccordionPanel from '../library/components/Containers/Accordion/AccordionPanel.jsx';
 import Label from '../library/components/Decoration/Label';
 import LabelBool from '../library/components/Decoration/LabelBool';
+import Button from '../library/components/Form/ButtonBool';
 
 export default class Bar extends React.Component {
     render() {
@@ -30,6 +31,23 @@ export default class Bar extends React.Component {
 	        	</AccordionPanel>
 
 	        	<AccordionPanel title="Accordion Panel Title Here 2">
+		        	<p>
+		            <Button href="#">
+		              This is a button
+		            </Button>
+		            <Button primary href="#">
+		              This is a button
+		            </Button>
+		            <Button secondary href="#">
+		              This is a button
+		            </Button>
+		            <Button danger href="#">
+		              This is a button
+		            </Button>
+		            <Button link href="#">
+		              This is a button
+		            </Button>
+		        	</p>
 	        		<p>
 	        			Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
 	        		</p>
