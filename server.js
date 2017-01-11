1af429a79f73844068bb47c33f671d761c39fe77
Ben Dunn <ben.dunn@parmenion.co.uk>
2017-01-11 12:48:21 +0000

    Change port to 3300

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./webpack.config.js');
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 3000 : process.env.PORT;
+const port = isDeveloping ? 3300 : process.env.PORT;
 const app = express();
 
 if (isDeveloping) {
@@ -28,19 +28,19 @@ if (isDeveloping) {
 
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
-  
+
   app.get('*', function response(req, res) {
     res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
     res.end();
   });
-  
+
 } else {
-  
+
   app.use(express.static(__dirname + '/dist'));
   app.get('*', function response(req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
-  
+
 }
 
 app.listen(port, '0.0.0.0', function onStart(err) {
