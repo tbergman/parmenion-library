4d01dfcfdddcc1d64d6b30a9352d1d5d5d08ed04
Ben Dunn <ben.dunn@parmenion.co.uk>
2017-01-17 10:36:48 +0000

    Bring back labels, rename to tags

--- app/containers/Bar.jsx
+++ app/containers/Bar.jsx
@@ -1,46 +1,42 @@
 import React from 'react';
-import { Accordion, AccordionPanel } from '../library/components/Containers/Accordion';
-import { Label } from '../library/components/Form/Label';
+import {
+  Accordion, AccordionPanel, Tag,
+} from 'library';
 
 export default props => (
   <Accordion>
-
     <AccordionPanel title="Accordion Panel Title Here">
       <p>
-        <Label>This is a label</Label>
-        <Label type={1}>This is a label</Label>
-        <Label type={2}>This is a label</Label>
-        <Label type={3}>This is a label</Label>
-        <Label type={4}>This is a label</Label>
+        <Tag>This is a label</Tag>
+        <Tag type={1}>This is a label</Tag>
+        <Tag type={2}>This is a label</Tag>
+        <Tag type={3}>This is a label</Tag>
+        <Tag type={4}>This is a label</Tag>
       </p>
       <p>
-          Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
-        </p>
+        Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
+      </p>
     </AccordionPanel>
-
     <AccordionPanel title="Accordion Panel Title Here 2">
       <p>
-          Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
-        </p>
+        Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
+      </p>
     </AccordionPanel>
-
     <AccordionPanel title="Accordion Panel Title Here 3">
       <p>
-          Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
-        </p>
+        Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
+      </p>
       <p>
-          Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
-        </p>
+        Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
+      </p>
       <p>
-          Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
-        </p>
+        Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
+      </p>
     </AccordionPanel>
-
     <AccordionPanel title="Accordion Panel Title Here 4">
       <p>
-          Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
-        </p>
+        Curabitur vitae mauris fringilla, sodales lacus placerat, volutpat enim. Fusce tristique nisi non metus lacinia commodo. Nullam ornare orci elit, non eleifend justo rutrum in. Curabitur consectetur gravida aliquet. Quisque et orci metus. Ut lacinia turpis ac arcu elementum, nec fringilla urna dictum. Phasellus porta elementum eros, in condimentum eros commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices justo ac lacus molestie, quis volutpat nulla pellentesque. Fusce vel pharetra odio. Nullam id ultrices elit, ac sodales risus. Nunc gravida aliquet ornare. Aliquam tristique, turpis at varius sagittis, elit magna posuere metus, ac tincidunt velit metus nec felis. Nullam eget nunc est. Sed fringilla, velit quis maximus feugiat, massa nulla tempus dui, eget cursus orci nisi quis neque. Donec venenatis erat lorem, non ultricies dui vehicula sit amet.
+      </p>
     </AccordionPanel>
-
   </Accordion>
 );
--- app/library/components/Decoration/index.js
+++ app/library/components/Decoration/index.js
@@ -1,2 +1,3 @@
 export * from './Icon';
 export * from './Tooltip';
+export * from './Tag';
